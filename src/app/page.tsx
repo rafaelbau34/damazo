d83be774dd1a48cb1c446eaa6682ae015086dce0
rafael <rafaelbaugar@gmail.com>
2025-02-11 14:31:34 -0600

    componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1>Hola</h1>;
+  return (
+    <>
+      <h1>Hola</h1>
+      <Button>Raton</Button>
+    </>
+  );
 }
